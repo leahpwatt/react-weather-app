fe2cda1ff71f69354709a0c3272912558590d6d7
Leah Watt <leahwatt@Leahs-MacBook-Pro.local>
2018-10-02 15:28:06 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,25 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './styles/App.css';
 
-// import Home from './components/Home';
 import CurrentTemp from './components/CurrentTemp';
 import WeeklyForecast from './components/WeeklyForecast';
 import Error from './components/Error';
 import Header from './components/Header';
 
 class App extends Component {
-  render() {
+  render() {    
     return (
       <BrowserRouter>
         <div>
           <Header />
           <Switch>
-            <Route path="/" exact component={CurrentTemp} />
-            <Route path="/5-day-forecast" component={WeeklyForecast} />
+            <Route 
+              path="/" 
+              exact 
+              component={CurrentTemp} />         
+            <Route 
+              path="/5-day-forecast" 
+              component={WeeklyForecast} />               
             <Route component={Error} />
           </Switch>        
         </div>
